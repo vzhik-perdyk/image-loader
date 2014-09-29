55bb9bbfb5243136d904c4bbce6788f29fc07da5
Ignat Loskutov <ignat.loskutov@gmail.com>
2014-09-29 22:01:41 +0400

    Everything works OK now; image search API changed to Flickr

--- app/src/main/java/ru/ifmo/md/lesson3/DisplayActivity.java
+++ app/src/main/java/ru/ifmo/md/lesson3/DisplayActivity.java
@@ -5,58 +5,172 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.GridLayout;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 
 
 /**
- * Created by ignat on 27.09.14.
+ * Лоскутов Игнат (2538), Некрасов Дмитрий (2538), Шолохов Алексей (2536)
  */
 public class DisplayActivity extends Activity {
 
-    LinearLayout ll;
+    private final static String imageLoaderPrefix = "https://api.datamarket.azure.com/Bing/Search/v1/Image?Query=%27";
+    private static int imagesDrawn = 0;
+    private GridLayout layout;
+    private GetImageTask getImageTask;
+    private ImageLoadingTask imageLoadingTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display_activity);
+        layout = (GridLayout)findViewById(R.id.display_activity);
         Intent thisIntent = getIntent();
-        String[] imageUrls = thisIntent.getStringArrayExtra("imageUrls");
         String translation = thisIntent.getStringExtra("translation");
+        String word = thisIntent.getStringExtra("word");
         TextView translationView = (TextView)findViewById(R.id.translationView);
         translationView.setText(translation);
-         ll = (LinearLayout)findViewById(R.id.display_activity);
-        for (int i = 0; i<10; i++) {
-            GetImageTask getimagetask = new GetImageTask();
-            getimagetask.execute(imageUrls[i]);
-        }
-
+        imageLoadingTask = new ImageLoadingTask();
+        imageLoadingTask.execute(word);
     }
 
-    class GetImageTask extends AsyncTask<String, Void, Drawable> {
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(imageLoadingTask != null) {
+            imageLoadingTask.cancel(true);
+        }
+        if(getImageTask != null) {
+            getImageTask.cancel(true);
+        }
+        Log.i("DisplayActivity", "destroyed((");
+    }
+
+    // Get images from Bing
+    /*private class ImageLoadingTask extends AsyncTask<String, Void, String[]> {
         @Override
-        protected Drawable doInBackground(String... word) {
+        protected String[] doInBackground(String... word) {
             try {
-                InputStream is = (InputStream)new URL(word[0]).getContent();
-                Drawable img = Drawable.createFromStream(is, "");
-                return img;
+                URL url = new URL(imageLoaderPrefix + URLEncoder.encode(word[0], "utf-8") + "%27");
+                Log.i("url", url.toString());
+                String accountKey = ":Vik8blAkuBkO6VyX8qJV+74lq79nNifdTToND5N8pQo=";
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty("Accept", "application/json");
+
+                byte[] accountKeyBytes = accountKey.getBytes("utf-8");
+                String accountKeyEnc = Base64.encodeToString(accountKeyBytes, Base64.DEFAULT);
+                connection.setRequestProperty("Authorization", "Basic " + accountKeyEnc);
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String response;
+                response = br.readLine();
+                connection.disconnect();
+
+                JSONObject jsonObject = new JSONObject(response);
+                JSONArray responses = jsonObject.getJSONObject("d").getJSONArray("results");
+                int resultsLength = Math.min(responses.length(), 10);
+                String[] results = new String[resultsLength];
+                for (int i = 0; i < resultsLength; i++) {
+                    results[i] = responses.getJSONObject(i).getString("MediaUrl");
+                }
+                for (String result : results) {
+                    Log.i("Response", "URL = " + result);
+                }
+                return results;
             } catch (Exception e) {
                 e.printStackTrace();
+                return null;
+            }
+        }
+    }*/
+
+    //Get images from Flickr
+    private class ImageLoadingTask extends AsyncTask<String, Void, String[]> {
+        @Override
+        protected String[] doInBackground(String... word) {
+            if(isCancelled())
+                return null;
+
+            try {
+                String url = ("https://api.flickr.com/services/rest/?method=flickr.photos.search&per_page=10&format=json&api_key=5c7c3f396db79009d2baecd139398d98&text=" + URLEncoder.encode(word[0], "utf-8"));
+                DefaultHttpClient httpClient = new DefaultHttpClient();
+                HttpResponse httpResponse = httpClient.execute(new HttpGet(url));
+                String response = EntityUtils.toString(httpResponse.getEntity());
+                response = response.substring(response.indexOf('{'), response.lastIndexOf(')'));
+                JSONObject jsonObject = new JSONObject(response);
+
+                JSONArray responses = jsonObject.getJSONObject("photos").getJSONArray("photo");
+                String[] results = new String[responses.length()];
+                for (int i = 0; i < results.length; i++) {
+                    JSONObject picture = responses.getJSONObject(i);
+                    results[i] = String.format("https://farm%d.staticflickr.com/%s/%s_%s_z.jpg",
+                            picture.getInt("farm"),
+                            picture.getString("server"),
+                            picture.getString("id"),
+                            picture.getString("secret")
+                    );
+                }
+                return results;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+        @Override
+        protected void onPostExecute(String[] imageUrls) {
+            if(imageUrls != null) {
+                for (String imageUrl : imageUrls) {
+                    getImageTask = new GetImageTask();
+                    getImageTask.execute(imageUrl);
+                }
+            }
+        }
+    }
+
+    private class GetImageTask extends AsyncTask<String, Void, Drawable> {
+        @Override
+        protected Drawable doInBackground(String... word) {
+            if(isCancelled())
+                return null;
+
+            try {
+                Log.i("lal", "tryin");
+                InputStream is = (InputStream) new URL(word[0]).getContent();
+                Drawable img = Drawable.createFromStream(is, "");
+                is.close();
+                return img;
+            } catch (Exception e) {
+                Log.i("lal", "i tried");
                 return null;
             }
         }
 
         protected void onPostExecute(Drawable d) {
             ImageView imgView = new ImageView(getApplicationContext());
+            imgView.setScaleType(ImageView.ScaleType.CENTER);
             imgView.setImageDrawable(d);
-            ll.addView(imgView);
+            GridLayout.Spec rowSpec = GridLayout.spec(imagesDrawn / 2, GridLayout.CENTER);
+            GridLayout.Spec colSpec = GridLayout.spec(imagesDrawn % 2, GridLayout.CENTER);
+            layout.addView(imgView, new GridLayout.LayoutParams(rowSpec, colSpec));
+            imagesDrawn++;
         }
     }
-
-
-
 }
--- app/src/main/java/ru/ifmo/md/lesson3/MainActivity.java
+++ app/src/main/java/ru/ifmo/md/lesson3/MainActivity.java
@@ -1,10 +1,11 @@
 package ru.ifmo.md.lesson3;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -14,30 +15,19 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
-import org.json.JSONArray;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.lang.Exception;
 import java.lang.Override;
 import java.lang.String;
 import java.lang.Void;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.net.URLEncoder;
 
 
 public class  MainActivity extends Activity {
 
     private final static String translatorPrefix = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20140924T073928Z.75f4072f7ba0940a.0e25b6e1b08d1c03dfb34d22a4055d8118e44d77&lang=en-ru&text=";
-    private final static String imageLoaderPrefix = "https://api.datamarket.azure.com/Bing/Search/v1/Image?Query=%27";
 
     private EditText editText;
-    private TranslationTask translationTask;
-    private ImageLoadingTask imageLoadingTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,81 +37,44 @@ public class  MainActivity extends Activity {
     }
 
     public void onClick(View view) {
-        translationTask = new TranslationTask();
-        translationTask.execute(editText.getText().toString());
+        new TranslationTask().execute(editText.getText().toString());
     }
 
-    class TranslationTask extends AsyncTask<String, Void, String> {
+    private class TranslationTask extends AsyncTask<String, Void, String> {
+        private String word;
         @Override
-        protected String doInBackground(String... word) {
+        protected String doInBackground(String... toTranslate) {
+            word = toTranslate[0];
             try {
-                String url = translatorPrefix + URLEncoder.encode(word[0], "utf-8");
+                String url = translatorPrefix + URLEncoder.encode(word, "utf-8");
                 DefaultHttpClient httpClient = new DefaultHttpClient();
                 HttpResponse httpResponse = httpClient.execute(new HttpGet(url));
                 String response = EntityUtils.toString(httpResponse.getEntity());
                 JSONObject jsonObject = new JSONObject(response);
-                //Log.i("lal", jsonObject.getJSONArray("text").getString(0));
                 return jsonObject.getJSONArray("text").getString(0);
             } catch (Exception e) {
-                e.printStackTrace();
                 return null;
             }
         }
         @Override
         protected void onPostExecute(String translation) {
-            imageLoadingTask = new ImageLoadingTask();
-            imageLoadingTask.execute(translation);
-            String[] imageUrls = null;
-            try {
-                imageUrls = imageLoadingTask.get();
-            } catch (Exception e) {
-                e.printStackTrace();
+            if(translation == null) {
+                new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Ошибка")
+                    .setMessage("Возникли проблемы с Интернет-соединением")
+                    .setNegativeButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        }).show();
+                return;
             }
             Intent displayIntent = new Intent(getApplicationContext(), DisplayActivity.class);
             displayIntent.putExtra("translation", translation);
-            displayIntent.putExtra("imageUrls", imageUrls);
+            displayIntent.putExtra("word", word);
+            Log.i("Main", "tryin to start displayactivity");
             startActivity(displayIntent);
-        }
-    }
-
-    class ImageLoadingTask extends AsyncTask<String, Void, String[]> {
-        @Override
-        protected String[] doInBackground(String... word) {
-            try {
-                URL url = new URL(imageLoaderPrefix + URLEncoder.encode(word[0], "utf-8") + "%27");
-                Log.i("url", url.toString());
-                String accountKey = ":Vik8blAkuBkO6VyX8qJV+74lq79nNifdTToND5N8pQo=";
-                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                connection.setRequestMethod("GET");
-                connection.setRequestProperty("Accept", "application/json");
-
-                byte[] accountKeyBytes = accountKey.getBytes("utf-8");
-                String accountKeyEnc = Base64.encodeToString(accountKeyBytes, Base64.DEFAULT);
-                connection.setRequestProperty("Authorization", "Basic " + accountKeyEnc);
-
-                BufferedReader br = new BufferedReader(new InputStreamReader(
-                        (connection.getInputStream())));
-                String response;
-                response = br.readLine();
-                connection.disconnect();
-//                PrintWriter writer = new PrintWriter("/sdcard/log.txt", "UTF-8");
-
-
-                JSONObject jsonObject = new JSONObject(response);
-                JSONArray responses = jsonObject.getJSONObject("d").getJSONArray("results");
-                String[] results = new String[10];
-                for (int i = 0; i < responses.length() && i < 10; i++) {
-                    results[i] = responses.getJSONObject(i).getString("MediaUrl");
-                }
-                for (int i = 0; i < results.length; i++) {
-                    Log.i("Response", "URL = " + results[i]);
-                }
-//                writer.println(response);
-                return results;
-            } catch (Exception e) {
-                e.printStackTrace();
-                return null;
-            }
         }
     }
 }
